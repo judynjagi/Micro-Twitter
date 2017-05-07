b22727e6df802ff5e536c7a777cbb7eef49bb073
Judy Njangi <judy.njagi@andela.com>
2017-05-07 18:27:54 +0300

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_path
+    # get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_path
+    # get static_pages_help_url
     assert_response :success
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test 'should get about' do
+    get about_path
+    # get static_pages_about_url
     assert_response :success
   end
-
+  test 'should get contact' do
+    get contact_path
+    # get static_pages_about_url
+    assert_response :success
+  end
 end
